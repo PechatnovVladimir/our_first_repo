94ac80b53c75e3298be1764316e47fe33dcb3c81
Vladimir Pechatnov <pechatnov@yandex.ru>
2024-10-04 15:43:09 +0700

    какой то коммит

---
+++ mymodule_BACKUP_1839525.py
@@ -0,0 +1,16 @@
+# Previously I had some complex logic in this
+# module, but I had to delete it due to reasons.
+# Very sad!
+
+def my_function (first, second):
+<<<<<<< HEAD
+    return 5 - 2
+=======
+    return 5+1
+>>>>>>> third
+
+def another_function(first):
+    return first+1
+
+def new_function():
+    print('I am a new function')
---
+++ mymodule_LOCAL_1839525.py
@@ -0,0 +1,12 @@
+# Previously I had some complex logic in this
+# module, but I had to delete it due to reasons.
+# Very sad!
+
+def my_function (first, second):
+    return 5 - 2
+
+def another_function(first):
+    return first+1
+
+def new_function():
+    print('I am a new function')
